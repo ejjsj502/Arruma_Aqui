14dca063e2520e832f201814b1e51b54b01e315f
ejjsj502 <pauloricardo00035@gmail.com>
2025-11-19 21:00:04 -0300

    Fix typo in STATICFILES_STORAGE setting

--- arruma/settings.py
+++ arruma/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
-TATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
