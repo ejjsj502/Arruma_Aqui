f9d452d342a5a4c5cac40eb4c99834fea5cf90b9
ejjsj502 <pauloricardo00035@gmail.com>
2025-11-19 21:42:10 -0300

    Update DATABASES setting to use environment variable

--- arruma/settings.py
+++ arruma/settings.py
@@ -83,10 +83,7 @@ WSGI_APPLICATION = 'arruma.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+   'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 
